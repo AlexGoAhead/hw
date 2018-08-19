1b5e6aa45afbd69c0e15bc503dba451c4e88ef14
AlexGoAhead <alex.8979323846@gmail.com>
2018-08-19 16:36:01 +0300

    first version

--- 1.py
+++ 1.py
@@ -1,1 +1,8 @@
-print('Hello world')
+#  Найти результат выражения для произвольных значений a,b,c: a + b * ( c / 2 )
+
+
+a = 5
+b = 10
+c = 15
+summary = a + b * (c/2)
+print('The result of equation is', summary)
