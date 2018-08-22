fd43b129ac5561893e284dd8c12ae9af81f4b246
AlexGoAhead <alex.8979323846@gmail.com>
2018-08-22 13:36:54 +0300

    7 is done

--- 7.py
+++ 7.py
@@ -1,5 +1,20 @@
 # Преобразовать строку с американским форматом даты в европейский. Например, '05.17.2016' преобразуется в '17.05.2016'
 from datetime import datetime
 
-dt = datetime.strptime('08.19.2018', '%m.%d.%Y')
-print(str(dt.strftime('%d.%m.%Y')))
+# dt = datetime.strptime('08.22.2018', '%m.%d.%Y')
+# print(str(dt.strftime('%d.%m.%Y')))
+
+
+
+import datetime
+
+d = datetime.datetime.now()
+b = str(d)
+m = b.split()[0]
+n = m.split('-')
+li = [n]
+# print(d)
+# print(m)
+print('USA date format is:', n[1] + '.' + n[2] + '.' + n[0])
+print('EU date format is:', n[2] + '.' + n[1] + '.' + n[0])
+
