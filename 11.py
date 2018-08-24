34d1d88b73c1f83c6fe5690dfa262205d95cfc5b
AlexGoAhead <alex.8979323846@gmail.com>
2018-08-24 15:58:45 +0300

    11 first try

---
+++ 11.py
@@ -0,0 +1,13 @@
+# Написать функцию, которая будет переводить градусы в радианы (без использования math.radians).
+# Используя эту функцию, вывести на экран значения косинусов углов в 60, 45 и 40 градусов.
+# def degrees2radians(degrees): # returns float
+# 			pass
+
+import math
+
+def degrees2radians(degrees):
+    return (degrees*math.pi)/180
+dgrs1 = degrees2radians(math.cos(60))
+dgrs2 = degrees2radians(math.cos(45))
+dgrs3 = degrees2radians(math.cos(40))
+print(dgrs1, dgrs2, dgrs3)
