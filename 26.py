d12b3a045dea7cb464a3dddda8df815c53f05272
AlexGoAhead <alex.8979323846@gmail.com>
2018-10-12 11:27:13 +0300

    remove 26 answer frm test_test

--- 26.py
+++ 26.py
@@ -10,57 +10,30 @@
 		для [1, 1, 1, 1, -1, 1, 1, -1, 0, 0, 0] надо вернуть 1
 
 """
+
 import random
-import collections
-from collections import Counter
+
 def calc_frequency(lst):
-    below_zero = []
-    zero = []
-    one = []
-    a = lst.count(1)
-    b = lst.count(-1)
-    c = lst.count(0)
-    if a > b and c:
-        print('number of 1 is: ', a)
-        return a
-    elif b > a and c:
-        print('number of -1 is: ', b)
-        return b
-    elif c > a and b:
-        print('number of 0 is: ', c)
-        return c
-    elif b == c:
+    elem_sum = [lst.count(-1), lst.count(0), lst.count(1)]
+    print(elem_sum)
+    below_zero = elem_sum[0]
+    zero = elem_sum[1]
+    one = elem_sum[2]
+    # b = list(a[2])
+    # print(a)
+    # print(b)
+    if elem_sum[0] == elem_sum[1] or elem_sum[0] == elem_sum[2] or elem_sum[1] == elem_sum[2]:
         print(None)
         return None
+    elif below_zero > zero and below_zero > one:
+        print(-1)
+        return -1
+    elif zero > below_zero and zero > one:
+        print(0)
+        return 0
+    elif one > below_zero or one > zero:
+        print(1)
+        return 1
     else:
-        print(None)
-        return None
-        if a == b or a == c or b == c or b == a or c == a:
-            print(None)
-            return None
-    print(lst)
-    print(a, b, c)
-ex = calc_frequency([1, 1, 1, -1, -1, 1, 1, -1, 0, 0, 0])
-# exmpl = calc_frequency([random.randint(-1, 1) for i in range(11)])
-
-#
-# Сколько
-# раз
-# входит
-# число
-# 7
-# в
-# список
-# a
-
-
-def mycount(a):
-    c = 0
-    for x in a:
-        if x == 7:
-            c = c + 1
-    return c
-
-
-a = [7, 9, -3, 7, 2, 1, 7]
-# print(mycount(a))
+        print('Error')
+res = calc_frequency([random.randint(-1, 1) for i in range(11)])
